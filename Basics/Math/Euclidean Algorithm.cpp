cf882d9483731489561cdf6d90898dbb9377b771
Hatary05 <77054303+Hatary05@users.noreply.github.com>
2025-07-06 09:31:57 +0900

    Create Euclidean Algorithm.cpp

---
+++ Basics/Math/Euclidean Algorithm.cpp
@@ -0,0 +1,18 @@
+ll extgcd(ll a, ll b, ll &x, ll &y){
+    ll u1 = 1, u2 = 0, v1 = 0, v2 = 1;
+    while(b){
+        ll q = a / b;
+        swap(a -= q*b, b);
+        swap(u1 -= q*u2, u2);
+        swap(v1 -= q*v2, v2);
+    }
+    x = u1, y = v1;
+    return a;
+};
+ll gcd(ll a, ll b){
+    while(b){
+        ll q = a / b;
+        swap(a -= q * b, b);
+    }
+    return a;
+};
