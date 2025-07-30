40f2d43514cd53761b2e2584cf46dae7f73b413c
Hatary05 <77054303+Hatary05@users.noreply.github.com>
2025-07-30 17:42:30 +0900

    Update all.cpp

--- Basics/Geometry/all.cpp
+++ Basics/Geometry/all.cpp
@@ -1,3 +1,22 @@
+inline ll ccw(pll a, pll b, pll c) {
+    return (b.fr-a.fr)*(c.sc-a.sc) - (c.fr-a.fr)*(b.sc-a.sc);
+}
+vector<pll> convex_hull(vector<pll> &points) {
+    sort(all(points)); compress(points);
+    if(sz(points) <= 2) return points;
+
+    vector<pll> up, low;
+    for(auto i : points) {
+        while(sz(up) >= 2 && ccw(up[sz(up)-2], up.back(), i) >= 0) up.pop_back();
+        while(sz(low) >= 2 && ccw(low[sz(low)-2], low.back(), i) <= 0) low.pop_back();
+        up.push_back(i), low.push_back(i);
+    }
+    reverse(all(up));
+    up.pop_back(); low.pop_back();
+    low.insert(low.end(), all(up));
+
+    return low;
+}
 bool P_in_the_Polygon(const vector<Pt>& P, const Pt& q) {
     int n = (int)P.size();
     if (n == 1) return (q.x == P[0].x && q.y == P[0].y);
