dd9c59f68504e97fd6ab7b865d009e6ae768b8d3
Hatary05 <77054303+Hatary05@users.noreply.github.com>
2025-07-31 16:28:03 +0900

    Update all.cpp

--- Basics/Geometry/all.cpp
+++ Basics/Geometry/all.cpp
@@ -1,6 +1,11 @@
 inline ll sgn(ll x) {return (x > 0) - (x < 0);}
 inline ll cross(pll a, pll b, pll c) {return (b.fr-a.fr)*(c.sc-a.sc) - (c.fr-a.fr)*(b.sc-a.sc);}
 inline ll ccw(pll a, pll b, pll c) {return sgn(cross(a, b, c));}
+bool on_seg(pll a, pll b, pll p) {
+    if(ccw(a, b, p) != 0) return false;
+    if(a > b) swap(a, b);
+    return (a <= p && p <= b);
+}
 bool seg_intersect(pll a, pll b, pll c, pll d){
     ll ab = ccw(a,b,c) * ccw(a,b,d);
     ll cd = ccw(c,d,a) * ccw(c,d,b);
