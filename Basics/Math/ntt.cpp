e2dea7b39e804bd4774a92703f35a52f8b3bbeda
Hatary05 <77054303+Hatary05@users.noreply.github.com>
2025-08-05 16:43:20 +0900

    Update ntt.cpp

--- Basics/Math/ntt.cpp
+++ Basics/Math/ntt.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define ld long double
 #define pii pair<int, int>
 #define pll pair<ll, ll>
 #define rep(i, n) for(int i = 0; i < (n); i++)
@@ -10,56 +11,55 @@ using namespace std;
 #define fr first
 #define sc second
 
-const int MOD = 998244353;
-const int ROOT = 3; //MOD의 원시근
-ll exp(ll x, ll e){
+ll exp_mod(ll x, ll e, ll MOD) {
     ll ret = 1;
-    while(e){
+    while(e) {
         if(e & 1) ret = ret * x % MOD;
         x = x * x % MOD; e >>= 1;
     }
     return ret;
 }
-void ntt(vector<ll>& a, bool invert){
+void ntt(vector<ll> &a, bool inv, ll MOD, ll root) {
     int n = sz(a);
-    for(int i = 1, j = 0; i < n; i++){
+    for(int i = 1, j = 0; i < n; i++) {
         int bit = n >> 1;
         for(; j & bit; bit >>= 1) j ^= bit;
         j ^= bit;
         if(i < j) swap(a[i], a[j]);
     }
 
-    for(int len = 2; len <= n; len <<= 1){
-        ll wlen = exp(ROOT, (MOD - 1) / len);
-        if(invert) wlen = exp(wlen, MOD - 2);
-        for(int i = 0; i < n; i += len){
+    for(int len = 2; len <= n; len <<= 1) {
+        ll wlen = exp_mod(root, (MOD - 1) / len, MOD);
+        if(inv) wlen = exp_mod(wlen, MOD - 2, MOD);
+
+        for(int i = 0; i < n; i += len) {
             ll w = 1;
-            for(int j = 0; j < len / 2; j++){
+            for(int j = 0; j < len / 2; j++) {
                 ll u = a[i + j];
                 ll v = a[i + j + len / 2] * w % MOD;
-                a[i + j] = (u + v < MOD) ? u + v : u + v - MOD;
-                a[i + j + len / 2] = (u - v >= 0) ? u - v : u - v + MOD;
+                a[i + j] = (u + v) % MOD;
+                a[i + j + len / 2] = (u - v + MOD) % MOD;
                 w = w * wlen % MOD;
             }
         }
     }
-    if(invert){
-        ll n_inv = exp(n, MOD - 2);
-        for(auto &x : a) x = x * n_inv % MOD;
+
+    if(inv) {
+        ll inv_n = exp_mod(n, MOD - 2, MOD);
+        for(auto &x : a) x = x * inv_n % MOD;
     }
 }
-vector<ll> convolution(const vector<ll>& a, const vector<ll>& b){
-    vector<ll> fa(all(a)), fb(all(b));
-    int n = sz(a) + sz(b) - 1;
-    int flag = 1;
-    for(; flag < n; flag <<= 1);
-    fa.resize(flag); fb.resize(flag);
+vector<ll> convolution(vector<ll> &a, vector<ll> &b, ll MOD, ll root) {
+    vector<ll> fa(a), fb(b);
+    int m = sz(a) + sz(b) - 1;
+    int n = 1 << (int)ceil(log2(m) - 1e-9);
+    fa.resize(n), fb.resize(n);
 
-    ntt(fa, false), ntt(fb, false);
-    rep(i, flag) fa[i] = fa[i] * fb[i] % MOD;
-    ntt(fa, true);
+    ntt(fa, false, MOD, root), ntt(fb, false, MOD, root);
+    rep(i, n) fa[i] = fa[i] * fb[i] % MOD;
+    ntt(fa, true, MOD, root);
 
-    fa.resize(n);
+    fa.resize(m);
     return fa;
 }
 int main(){
@@ -70,20 +70,29 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    int n; cin >> n;
-    vector<ll> a(n), b(n);
-    rep(i, n) cin >> a[i];
-    rep(i, n) cin >> b[i];
+    int n, m; cin >> n >> m;
+    vector<ll> a(n + 1), b(m + 1);
+    rep(i, n + 1) cin >> a[i];
+    rep(j, m + 1) cin >> b[j];
 
-    reverse(all(a));
-    b.insert(b.end(), all(b));
-    vector<ll> conv = convolution(b, a);
+    const ll P1 = 998244353;
+    const ll ROOT1 = 3;
+    const ll P2 = 1004535809;
+    const ll ROOT2 = 3;
+    const ll INV_P1_MOD_P2 = 669690699;
+
+    auto c1 = convolution(a, b, P1, ROOT1);
+    auto c2 = convolution(a, b, P2, ROOT2);
 
     ll ans = 0;
-    for(int i = n - 1; i <= 2*(n + 1); i++){
-        if(i >= sz(conv)) break;
-        ans = max(ans, conv[i]);
+    int k = sz(c1);
+    for(int i = 0; i < k; i++) {
+        ll t = (c2[i] - c1[i] + P2) % P2;
+        t = t * INV_P1_MOD_P2 % P2;
+        ll ck = c1[i] + P1 * t;
+
+        ans ^= ck;
     }
-    cout << ans << endl;
+    cout << ans << '\n';
     return 0;
 }
